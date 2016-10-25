560dfe5b93f3ad8fa519d6738be433fb7e19cccd
Lasithd2 <lasithd2@gmail.com>
2016-10-25 23:58:08 +0530

    db.php

--- adminPanel/ordering/includes/db.php
+++ adminPanel/ordering/includes/db.php
@@ -2,16 +2,25 @@
 
 //Defining variables for the database
 
-$dbhost="localhost";
-$dbuser="User";
-$dbpassword="abc123";
-$dbname="project1";
+$dbhost="us-cdbr-azure-southcentral-f.cloudapp.net";
+$dbuser="bd3bc2a79fa1ad";
+$dbpassword="60eb0f4f";
+$dbname="acsm_2a0677acd5113ea";
 
 //function to connect to the database
 $db = mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
 
 
 
+if ($db) {
+
+
+}
+else {
+
+print "Unsuccessful Database connection <br>";
+
+}
 
 
 
