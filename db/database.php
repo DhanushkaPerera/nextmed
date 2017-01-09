f10354985479b310a2c63b88dee768279a7ad4d0
AmayaMashi <amayaisnoww@gmail.com>
2017-01-09 15:29:11 +0530

    changed the database

--- db/database.php
+++ db/database.php
@@ -1,31 +1,3 @@
 <?php
-//$con = mysqli_connect("localhost","root","","Trainee_quiz");
-
-
-
-//Defining variables for the database
-
-$dbhost="us-cdbr-azure-southcentral-f.cloudapp.net";
-$dbuser="bd3bc2a79fa1ad";
-$dbpassword="60eb0f4f";
-$dbname="acsm_2a0677acd5113ea";
-
-//function to connect to the database
-$con = mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
-
-
-
-if ($con) {
-
-}
-else {
-
-print "Unsuccessful Database connection <br>";
-
-}
-
-
-
-
-
+$con = mysqli_connect("localhost","root","","Trainee_quiz");
 ?>
