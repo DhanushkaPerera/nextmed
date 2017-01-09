954cecd0cda19607c55c803a6df3c10110291ef9
Dhanushka Perera <dhanushka93u@gmail.com>
2017-01-08 21:01:15 -0800

    auto +1

--- adminPanel/Bill/bill.php
+++ adminPanel/Bill/bill.php
@@ -4,7 +4,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Billing</title>
     <link rel="stylesheet" type="text/css" href="billnew.css">
-	<script src="../../jquery/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="style.css">
+<script src="../../jquery/jquery.min.js"></script>
 
 
 </head>
@@ -96,7 +97,97 @@ if(isset($_POST["search"])){
 			<input type="radio" onfocus="headingBoxActive('hBoxOM')" autofocus name=ReturnPolicy onfocusout="validatePhoneNo(this, 'returnperror');validatedAll();"/>General<br>
 			</div>
 		
-		<div class="headingbox" id="hBoxBRAND" > Brand Name </div>
+		<div class="column">
+                    <div class="headingbox" id="hBoxNIC" > Brand Name  </div>
+                    <div class="inputboxWrap">
+                        <input type="text" placeholder="" maxlength="13" name=nic onfocus="headingBoxActive('hBoxNIC')" onkeyup="validateNIC(this)"   onfocusout="upperCASE(this);hide('NICerror');validateoutNIC(this);validatedAll();" />
+                    </div>
+                    <div class="poperror" id="NICerror" ></div>
+                    <div class="error" id="nicerror2" > error occured </div><br>
+
+                    
+
+
+                </div>
+
+                <div class="column" style="clear: right;">
+                     <!-- user First Name input -->
+                    <div class="headingbox" id="hBoxFN" >   Generic Name </div>
+                    <div class="inputboxWrap" >
+                        <input type="text"   name=firstname onfocus="headingBoxActive('hBoxFN')" onkeydown="validateString(this, 'nameerror')" onfocusout="hide('nameerror');validateoutString(this, 'nameerror2');validatedAll();" /><br>
+                    </div>
+                    <div class="poperror" id="nameerror" ></div>
+                    <div class="error" id="nameerror2" > error occured </div><br>
+
+
+                    
+
+
+                </div>
+				
+				<div class="column">
+                    <div class="headingbox" id="hBoxNIC" > Dosage Form </div>
+                    <div class="inputboxWrap">
+                        <select>
+						<option value="Capsule">Dosage Form</option>
+							<option value="Capsule">Capsule</option>
+	<option value="Tablet">Tablet</option>
+	<option value="Pill">Pill</option>
+	<option value="Syrup">Syrup</option>
+	<option value="Cream">Cream</option>
+	<option value="Liquid">Liquid</option>
+	<option value="Gel">Gel</option>
+	<option value="Balm">Balm</option>
+	<option value="Lotion">Lotion</option>
+	<option value="Ointment">Ointment</option>
+	<option value="Ear drops">Ear drops</option>
+	<option value="Eye drops">Eye drops</option>
+</select>
+						
+                    </div>
+                    <div class="poperror" id="NICerror" ></div>
+                    <div class="error" id="nicerror2" > error occured </div><br>
+
+                    
+
+
+                </div>
+				
+				<div class="column">
+                    <div class="headingbox" id="hBoxNIC" > Strength </div>
+                    <div class="inputboxWrap">
+                        <select name=Strength  onfocus="headingBoxActive('hBoxSTR')" onfocusout="headingBoxInactive('hBoxSTR')" >
+	<option value="" disabled selected  > Strength </option>
+	<option value="10mg">10mg</option>
+	<option value="10ml">10ml</option>
+	<option value="1spoon">1spoon</option>
+	<option value="1drop">1drop</option>
+	<option value="1capsule">1capsule</option>
+	<option value="1tablet">1tablet</option>
+	<option value="1pill">1pill</option>
+	</select>
+                    </div>
+                    <div class="poperror" id="NICerror" ></div>
+                    <div class="error" id="nicerror2" > error occured </div><br>
+
+                    
+
+
+                </div>
+
+                
+
+                <div style="width:100%;text-align:center; " >
+                
+
+                <input class="buttonDis"  type="submit" name=submit value=Search id="submitButton" disabled onclick="validatedAll();parent.parent.closeIFrame();" >
+				                <button class="buttonS" onclick="parent.parent.closeIFrame();" > Cancel </button>
+
+                </div>
+
+		</div>
+		
+		<!--div class="headingbox" id="hBoxBRAND" > Brand Name </div>
 			<div style="width:100%;text-align:center;">
             <input type="text" autofocus name=brandname onfocus="headingBoxActive('hBoxBRAND')" onkeydown="validateString(this, 'nameerror')" onfocusout="hide('nameerror');validateoutString(this, 'nameerror2');validatedAll();" /><br>
 			</div>
@@ -112,7 +203,7 @@ if(isset($_POST["search"])){
 				<input class="btnSearch"  type="submit" name=submit value=SUBMIT id="searchButton" >
 			</div>
 			
-		</div>
+		</div-->
 						
 	</div>
 	
@@ -121,7 +212,7 @@ if(isset($_POST["search"])){
 
 <script>
 $( document ).ready(function() {
-	alert('test');
+	/*alert('test');*/
 function autoNum(){
 		var last = "<?php $sql='select InvoiceNo from invoice where InvoiceNo = (select max(InvoiceNo) from invoice)';
 		$res = mysqli_query($db,$sql);
@@ -129,9 +220,10 @@ function autoNum(){
 				while($array = mysqli_fetch_array($res)){
             
 		echo $array[0];}}?>";
-			alert(last)
+			/*alert(last*/
 		var inputBox = document.getElementById('order');
-		inputBox.value=last;
+		var lastnum=Number(last);
+		inputBox.value=lastnum+1;
 
 }
 autoNum();
