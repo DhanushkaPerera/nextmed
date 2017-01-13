fc789b7d135badd0d7e2b76aeba65208ce0cc045
Lasithd2 <lasithd2@gmail.com>
2017-01-13 22:39:02 +0530

    colour

--- adminPanel/ordering/report.php
+++ adminPanel/ordering/report.php
@@ -22,22 +22,25 @@ $result=mysqli_query($db,$sql);
 $pdf=new PDF();
 $pdf->AddPage();
 $pdf->AliasNbPages();
-$pdf->SetFont('Arial','B',12);
+$pdf->SetFont('Arial','B',16);
 
-$pdf->Cell(90,10,'Date',1,0,'C',0);
-$pdf->Cell(90,10,'Number of online orders',1,1,'C',0);
+$pdf->SetFillColor(51,153,255);
+$pdf->Cell(90,15,'Date',1,0,'C',1);
+$pdf->Cell(90,15,'Number of orders',1,1,'C',1);
  
 
 while($row=mysqli_fetch_array($result))
 {
   
   // cell with left and right borders
-
-$pdf->Cell(90,10,$row['Date'],1,0,'C',0); // 1,0 
-$pdf->Cell(90,10,$row['Number'] ,1,1,'C',0);
+$pdf->SetFont('Arial','B',13);	
+$pdf->SetFillColor(153,153,255);
+$pdf->Cell(90,10,$row['Date'],1,0,'C',1); // 1,0 
+$pdf->Cell(90,10,$row['Number'] ,1,1,'C',1);
    
    
 }
+
 
 $pdf->output();
 
